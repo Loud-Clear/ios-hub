74b6c1991846563640753ff752524d72156890e9
Ivan Zezyulya <ivan@zezyulya.com>
2016-11-30 13:39:57 +0600

    Added Macroses/CCTypedefs.h

---
+++ Components/Macroses/CCTypedefs.h
@@ -0,0 +1,11 @@
+//
+//  CCTypedefs.h
+//  iOS Hub
+//
+//  Created by Ivan Zezyulya on 30/11/2016.
+//  Copyright (c) 2016 Loud & Clear. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef void(^CCGenericBlock)(NSError *error);
