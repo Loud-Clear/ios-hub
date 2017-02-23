8eebe73e0fc765a0a988fbf81c743702089a2eb7
Aleksey Garbarev <alex.garbarev@gmail.com>
2017-02-23 20:11:27 +0600

    trying to make flexible desrializing/deserializing api

--- Components/RealmAddons/CCPersistentModel.h
+++ Components/RealmAddons/CCPersistentModel.h
@@ -13,12 +13,18 @@
 #import "RLMObject+ThreadSafety.h"
 
 @class CCPersistentId;
+@class TyphoonTypeDescriptor;
 
 @interface CCPersistentModel : RLMObject
 
 + (NSArray *)serializedProperties;
 
 - (void)beforeAddOrUpdate:(RLMRealm *)realm;
+
+//Serialization customization
++ (Class)classForStorageForProperty:(NSString *)name;
+- (id)deserializeValue:(id)serializedValue forPropertyName:(NSString *)name type:(TyphoonTypeDescriptor *)type;
+- (id)serializeValue:(id)value forPropertyName:(NSString *)name type:(TyphoonTypeDescriptor *)type;
 
 @end
 
