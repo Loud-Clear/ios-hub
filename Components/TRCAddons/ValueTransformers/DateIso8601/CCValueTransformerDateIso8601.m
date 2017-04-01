de11c0da941760c8a8c55a875ce80d2bce013b6a
Ivan <ivan@MacPro.local>
2017-04-01 14:59:40 +0600

    Components/TRCAddons: make CCValueTransformerDateIso8601 to produce time in UTC instead of lolca time for requests

--- Components/TRCAddons/ValueTransformers/DateIso8601/CCValueTransformerDateIso8601.m
+++ Components/TRCAddons/ValueTransformers/DateIso8601/CCValueTransformerDateIso8601.m
@@ -15,23 +15,42 @@
 
 @implementation CCValueTransformerDateIso8601
 
-+ (NSDateFormatter *)sharedDateFormatter
++ (NSDateFormatter *)requestDateFormatter
 {
-    static NSDateFormatter *dateFormatter = nil;
+    static NSDateFormatter *formatter = nil;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        dateFormatter = [[NSDateFormatter alloc] init];
-        [dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ssZ"];
-        NSLocale *posix = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"];
-        [dateFormatter setLocale:posix];
+        formatter = [NSDateFormatter new];
+        formatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ss'Z'";
+        formatter.locale = [NSLocale localeWithLocaleIdentifier:@"en_US_POSIX"];
+        formatter.timeZone = [NSTimeZone timeZoneWithAbbreviation:@"UTC"];
     });
-    return dateFormatter;
+    return formatter;
+}
+
++ (NSDateFormatter *)responseDateFormatter
+{
+    static NSDateFormatter *formatter = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        formatter = [NSDateFormatter new];
+        formatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ssZ";
+        formatter.locale = [NSLocale localeWithLocaleIdentifier:@"en_US_POSIX"];
+    });
+    return formatter;
 }
 
 - (NSDate *)objectFromResponseValue:(NSString *)responseValue error:(NSError **)error
 {
-    NSDateFormatter *dateFormatter = [[self class] sharedDateFormatter];
-    
+    if (![responseValue isKindOfClass:[NSString class]]) {
+        if (error) {
+            *error = TRCErrorWithFormat(TyphoonRestClientErrorCodeRequestSerialization, @"Expected 'NSString' object, but got '%@'.", NSStringFromClass([responseValue class]));
+        }
+        return nil;
+    }
+
+    NSDateFormatter *dateFormatter = [[self class] responseDateFormatter];
+
     NSDate *date = [dateFormatter dateFromString:responseValue];
     if (!date && error) {
         *error = TRCErrorWithFormat(TyphoonRestClientErrorCodeResponseSerialization, @"Can't create NSDate from string '%@'", responseValue);
@@ -47,14 +66,14 @@
         }
         return nil;
     }
-    NSDateFormatter *dateFormatter = [[self class] sharedDateFormatter];
-    
+    NSDateFormatter *dateFormatter = [[self class] requestDateFormatter];
+
     NSString *string = [dateFormatter stringFromDate:object];
-    
+
     if (!string && error) {
         *error = TRCErrorWithFormat(TyphoonRestClientErrorCodeRequestSerialization, @"Can't convert NSDate '%@' into NSStrign", object);
     }
-    
+
     return string;
 }
 
